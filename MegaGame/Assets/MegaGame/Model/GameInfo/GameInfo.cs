281ced70ee2004024abaeea99b8fca3a199f44ac
SamSchreiber96 <42520755+SamSchreiber96@users.noreply.github.com>
2019-11-09 22:00:00 -0800

    serialize/deserialize

--- MegaGame/Assets/MegaGame/Model/GameInfo/GameInfo.cs
+++ MegaGame/Assets/MegaGame/Model/GameInfo/GameInfo.cs
@@ -1,8 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 public class GameInfo
 {
+    public byte Id { get; set; }
+
+    public static byte[] Serialize(object o)
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        using (var ms = new System.IO.MemoryStream())
+        {
+            bf.Serialize(ms, o);
+            return ms.ToArray();
+        }
+    }
+
+    public static object Deserialize(byte[] arrBytes)
+    {
+        using (var memStream = new System.IO.MemoryStream())
+        {
+            var binForm = new BinaryFormatter();
+            memStream.Write(arrBytes, 0, arrBytes.Length);
+            memStream.Seek(0, System.IO.SeekOrigin.Begin);
+            var obj = binForm.Deserialize(memStream);
+            return obj;
+        }
+    }
     public Dictionary<string, EntityInfo> entityInfos = new Dictionary<string, EntityInfo>();
 }
