214b6f3e944d1e57641f7a18f14cf3ead0ebbad7
SamSchreiber96 <42520755+SamSchreiber96@users.noreply.github.com>
2019-11-09 16:50:06 -0800

    Add classes for the Model

---
+++ MegaGame/Assets/MegaGame/Model/TileEntities/PlayerTileEntity.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MegaGame
+{
+    public class PlayerTileEntity : MoveableTileEntity
+    {
+        private bool cooldown = false;
+
+        public static float COOLDOWN_TIME = 0.4f;
+        public PlayerTileEntity(GameBoard gb, string name, float maxHealth) : base(gb, name, maxHealth)
+        {
+
+        }
+
+        public override void DoMove(Direction direction, int distance)
+        {
+            gameBoard.Move(this, direction, 1);
+        }
+
+        public void DoMove(Direction direction)
+        {
+            gameBoard.Move(this, direction, 1);
+        }
+
+        public override void DoTick()
+        {
+            // Do nothing
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        public void ResetCooldown()
+        {
+            cooldown = false;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (!cooldown)
+            {
+                if (Input.GetKeyDown("w"))
+                {
+                    DoMove(Direction.UP);
+                }
+                else if (Input.GetKeyDown("d"))
+                {
+                    DoMove(Direction.RIGHT);
+                }
+                else if (Input.GetKeyDown("a"))
+                {
+                    DoMove(Direction.LEFT);
+                }
+                else if (Input.GetKeyDown("s"))
+                {
+                    DoMove(Direction.DOWN);
+                }
+                Invoke("ResetCooldown", COOLDOWN_TIME);
+                cooldown = true;
+            }
+        }
+
+
+    }
+}
---
+++ MegaGame/Assets/MegaGame/Model/Tiles/NeutralTile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MegaGame
+{
+    public class NeutralTile : Tile
+    {
+
+        public NeutralTile(GameBoard gb, SIDE side) : base(gb, side)
+        {
+
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public override void ApplyEffect(TileEntity tileEntity)
+        {
+
+        }
+
+        public override void DoTick()
+        {
+        }
+    }
+}
