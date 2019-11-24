f003498f88df31ee02f53953aa6928198aa0bf57
SamSchreiber96 <42520755+SamSchreiber96@users.noreply.github.com>
2019-11-23 16:58:41 -0800

    Revert "Revert "Added TimedActionManager for executing tasks which occur periodically""
    This reverts commit 90413b192ac0366d2857400ed22be738fc933e55.

---
+++ MegaGame/Assets/MegaGame/Scripts/Model/Actions/Action.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MegaGame
+{
+    public abstract class Action
+    {
+        public abstract void DoAction();
+    }
+}
---
+++ MegaGame/Assets/MegaGame/Scripts/Model/TileEntities/ShockWaveEntity.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MegaGame
+{
+    public class ShockWaveEntity : DamageTileEntity
+    {
+        public static float SHOCKWAVE_SPEED = 300;
+        private class ShockWaveAction : Action
+        {
+            public override void DoAction()
+            {
+            }
+        }
+
+        public ShockWaveEntity(GameBoard gb, string name, int maxHealth) : base(gb, name, maxHealth)
+        {
+            TimedAction action = new TimedAction(new ShockWaveAction(), SHOCKWAVE_SPEED);
+            TimedActionManager.GetInstance().RegisterAction(action, this);
+        }
+
+        public override void DoTick()
+        {
+        }
+    }
+}
